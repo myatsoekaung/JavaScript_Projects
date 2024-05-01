f92817245d7739df59df14562790efb563e2ae86
Myat Soe Kaung <myatsoekaung0000@gmail.com>
2024-05-01 16:33:43 +0630

    js day 3

---
+++ loan_calculator/script.js
@@ -0,0 +1,26 @@
+document.getElementById('calculateBtn').addEventListener('click',calculateLoan);
+
+function calculateLoan(){
+    const loanAmount = parseFloat(document.getElementById('loanAmountInput').value);
+    const interestRate = parseFloat(document.getElementById('interestRateInput').value);
+    const loanTerm = parseFloat(document.getElementById('loanTermInput').value);
+
+    if(isNaN(loanAmount) || isNaN(interestRate) || isNaN(loanTerm)){
+        alert("Please enter valid numbers for all fields");
+    }
+
+    const monthlyInterest = interestRate / 100 / 12;
+    const totalPayment = loanTerm;
+    const monthlyPayment = (loanAmount * monthlyInterest) / (1-Math.pow(1 + monthlyInterest, -totalPayment));
+    const totalInterest = (monthlyPayment * totalPayment) - loanAmount;
+
+    displayResult(monthlyPayment,totalInterest);
+}
+
+function displayResult(monthlyPayment,totalInterest){
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = `
+        <p> <strong>Monthly Payment: ${monthlyPayment.toFixed(2)}</p> </strong>
+        <p> <strong>Monthly Payment: ${totalInterest.toFixed(2)}</p> </strong>
+    `;
+}
